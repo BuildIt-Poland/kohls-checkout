92b053e0ace240995577e9df8d39c48403ccf81b
Maciej Hein <maciej.hein@wipro.com>
2019-03-27 15:29:39 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 # Install the AWS CLI so we can publish to S3
 # --upgrade option tells to upgrade any requirements that are already installed. 
 # --user option tells to install the program to a subdirectory of your user directory to avoid modifying 
