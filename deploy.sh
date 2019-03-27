badebde7ac76391f07a40865b9c9a0edd7e3979a
Maciej Hein <maciej.hein@wipro.com>
2019-03-27 15:04:01 +0100

    fix-3

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 # --upgrade option tells to upgrade any requirements that are already installed. 
 # --user option tells to install the program to a subdirectory of your user directory to avoid modifying 
 # libraries used by your operating system.
-#pip install awscli --upgrade --user
+pip install awscli --upgrade --user
 
 # Sync our build folder with our S3 bucket
 # --acl public-read says deploy the files with public read access
 # --delete says to delete files in the bucket that aren't present in the build folder
-#aws s3 sync build/ "s3://buildit-store" --acl public-read --delete
+aws s3 sync build/ "s3://buildit-store" --acl public-read --delete
