f57172724d554fd89cdc71fe030c793e2aca64ae
Luciano Santos <luciusjss@gmail.com>
2019-03-28 15:49:36 +0100

    Styled componenets theme added

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -7,17 +7,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from '../App';
 import GlobalStyle from '../GlobalStyle';
+import theme from '../../styles/theme';
 
 function Root({ store }) {
   return (
     <Provider store={store}>
       <GlobalStyle />
-      <Router>
-        <App />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  // Colors
+  colorWhite: '#ffffff',
+  colorTurquoise: '#00a09f',
+
+  // Radius
+  bordeRadiusCircle: '50%',
+  borderRadiusMedium: '0.25rem'
+};
+
+export default theme;
