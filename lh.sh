fc521d2d75bb721fe5761bf064e7ca490155784b
Marcin Goławski <marcin.golawski@wipro.com>
2019-03-29 15:46:05 +0100

    test

--- lh.sh
+++ lh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "$TRAVIS_PULL_REQUEST"
+echo "$TRAVIS_BRANCH"
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "feature/lighthouse-ci" ]; then
   echo "test1"
   npm run lh
