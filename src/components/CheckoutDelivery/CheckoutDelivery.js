d01378c2028047129eaf14098bddfc3d1081476a
Marcin Goławski <marcin.golawski@wipro.com>
2019-04-01 14:32:27 +0200

    Revert checkout delivery changes

--- src/components/CheckoutDelivery/CheckoutDelivery.js
+++ src/components/CheckoutDelivery/CheckoutDelivery.js
@@ -6,7 +6,9 @@ import NextStep from '../NextStep';
 function CheckoutDelivery() {
   return (
     <>
-      <section />
+      <section>
+        <h3>CheckoutDelivery placeholder</h3>
+      </section>
       <NextStep label="Continue to Payment" to={CHECKOUT_PAYMENT_PATH} />
     </>
   );
