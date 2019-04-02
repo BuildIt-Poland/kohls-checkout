74c260ea0dda0014b1d06f7d05a973054ce4739a
blurbyte <mateusz.lachacz@wipro.com>
2019-04-02 10:28:42 +0200

    Add LandingPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import { CHECKOUT_PATH, CART_PATH } from '../../constants/routes';
+import { ROOT_PATH, CHECKOUT_PATH, CART_PATH } from '../../constants/routes';
 import Header from '../Header';
+import MainContent from './MainContent';
+import LandingPage from '../LandingPage';
 import Cart from '../Cart';
 import Checkout from '../Checkout';
 
 function App() {
   return (
-    <section>
+    <>
       <Header />
-      <Route exact path={CART_PATH} component={Cart} />
-      <Route exact path={CHECKOUT_PATH} component={Checkout} />
-    </section>
+      <MainContent>
+        <Route exact path={ROOT_PATH} component={LandingPage} />
+        <Route exact path={CART_PATH} component={Cart} />
+        <Route exact path={CHECKOUT_PATH} component={Checkout} />
+      </MainContent>
+    </>
   );
 }
 
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Content from '../Content';
+
+function LandingPage() {
+  return (
+    <Content>
+      <h1>Project goals</h1>
+    </Content>
+  );
+}
+
+export default LandingPage;
