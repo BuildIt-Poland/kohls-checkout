1a296e025fd78bc7944e27900680ce11a10a5c00
blurbyte <mateusz.lachacz@wipro.com>
2019-04-04 13:37:39 +0200

    Add Cross svg icon

---
+++ src/components/Icons/Cross.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Cross() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19">
+      <path
+        fill="#b0b0b0"
+        d="M2.54 19.064l6.987-6.986 6.986 6.987 2.475-2.475-6.987-6.987 6.987-6.986L16.513.142 9.527 7.128 2.541.142.065 2.617l6.987 6.986-6.986 6.986z"
+      />
+    </svg>
+  );
+}
+
+export default Cross;
