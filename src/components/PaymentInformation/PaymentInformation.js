64e91ddbac26abe075fc2f24b0a3e71ee23759d1
Marcin Goławski <marcin.golawski@wipro.com>
2019-04-04 11:13:54 +0200

    Change children composition for prop composition for action element

--- src/components/PaymentInformation/PaymentInformation.js
+++ src/components/PaymentInformation/PaymentInformation.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { CHECKOUT_PAYMENT_PATH } from '../../constants/routes';
-import TextRow from '../OrderSummary/TextRow';
 import TextLink from '../TextLink/TextLink';
 import SectionHeader from '../SectionHeader';
 
 function PaymentInformation() {
+  const textLink = (
+    <TextLink as={Link} to={CHECKOUT_PAYMENT_PATH}>
+      Edit
+    </TextLink>
+  );
+
   return (
     <section>
-      <SectionHeader>
-        <TextRow>
-          Payment Information
-          <TextLink as={Link} to={CHECKOUT_PAYMENT_PATH}>
-            Edit
-          </TextLink>
-        </TextRow>
-      </SectionHeader>
+      <SectionHeader actionElement={textLink}>Payment Information</SectionHeader>
     </section>
   );
 }
--- src/components/ShippingDetails/ShippingDetails.js
+++ src/components/ShippingDetails/ShippingDetails.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { CHECKOUT_DELIVERY_PATH } from '../../constants/routes';
-import TextRow from '../OrderSummary/TextRow';
 import SectionHeader from '../SectionHeader';
 import TextLink from '../TextLink';
 
 function ShippingDetails() {
+  const textLink = (
+    <TextLink as={Link} to={CHECKOUT_DELIVERY_PATH}>
+      Edit
+    </TextLink>
+  );
+
   return (
     <section>
-      <SectionHeader>
-        <TextRow>
-          Shipping Details
-          <TextLink as={Link} to={CHECKOUT_DELIVERY_PATH}>
-            Edit
-          </TextLink>
-        </TextRow>
-      </SectionHeader>
+      <SectionHeader actionElement={textLink}>Shipping Details</SectionHeader>
     </section>
   );
 }
