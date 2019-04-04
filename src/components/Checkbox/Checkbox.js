589921e9e1c5c7618915916b5c5caa10ccec78e3
Maciej Hein <maciej.hein@wipro.com>
2019-04-04 08:47:09 +0200

    remove test id

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -9,10 +9,10 @@ import Tick from './Tick';
 function Checkbox({ label, checked, onChange }) {
   return (
     <Wrapper>
-      <Input data-testid="checkbox-input" type="checkbox" checked={checked} onChange={onChange} />
+      <Input type="checkbox" checked={checked} onChange={onChange} />
       <CheckMark />
       <Label>{label}</Label>
-      {checked && <Tick data-testid="checkbox-tick" />}
+      {checked && <Tick />}
     </Wrapper>
   );
 }
