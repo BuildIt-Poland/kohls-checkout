c4427090c6bcd885a95107d4c69ea7fd04c0b31e
Luciano Santos <luciusjss@gmail.com>
2019-04-05 10:40:47 +0200

    Added new empty line shippingDetails component

--- src/components/ShippingDetails/ShippingDetails.js
+++ src/components/ShippingDetails/ShippingDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
 import Shipping from '../Icons/Shipping';
 import Text from '../Text';
 import { colorLightGrey } from '../../styles/designTokens';
