e2b9f0568de4c8d3fc13e841a87743bfad41cc99
Luciano Santos <luciusjss@gmail.com>
2019-04-08 11:30:52 +0200

    fix unit tests input field

--- src/components/Input/__tests__/Input.test.js
+++ src/components/Input/__tests__/Input.test.js
@@ -5,66 +5,40 @@ import Input from '../Input.js';
 
 describe('COMPONENT - Input', () => {
   describe('Input without error', () => {
-    let component;
-
-    beforeEach(() => {
+    it('renders correctly Input', () => {
       const props = {
         label: 'Dummy Label',
         placeholder: 'Dummy placeHolder',
         type: 'text'
       };
+      const component = create(<Input {...props} />);
 
-      component = create(<Input {...props} />);
-    });
-
-    afterEach(() => {
-      component.unmount();
-    });
-
-    it('renders correctly Input', () => {
       expect(component.toJSON()).toMatchSnapshot();
     });
   });
 
   describe('Input with error', () => {
-    let component;
-
-    beforeEach(() => {
+    it('renders correctly Input', () => {
       const props = {
         label: 'Dummy Label',
         placeholder: 'Dummy placeHolder',
         type: 'text',
         error: 'dummyError'
       };
+      const component = create(<Input {...props} />);
 
-      component = create(<Input {...props} />);
-    });
-
-    afterEach(() => {
-      component.unmount();
-    });
-
-    it('renders correctly Input', () => {
       expect(component.toJSON()).toMatchSnapshot();
     });
   });
 
   describe('Input without label', () => {
-    let component;
-
-    beforeEach(() => {
+    it('renders correctly Input', () => {
       const props = {
         placeholder: 'Dummy placeHolder',
         type: 'text'
       };
-      component = create(<Input {...props} />);
-    });
+      const component = create(<Input {...props} />);
 
-    afterEach(() => {
-      component.unmount();
-    });
-
-    it('renders correctly Input', () => {
       expect(component.toJSON()).toMatchSnapshot();
     });
   });
