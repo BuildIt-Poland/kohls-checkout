25202eae07638afa7102fb5b01e44d42eb1bda47
Maciej Hein <maciej.hein@wipro.com>
2019-04-08 17:07:18 +0200

    changing page title

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,12 +1,21 @@
 // Generic page wrapper
-
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import { spacingLarge } from '../../styles/designTokens';
 
-const Page = styled.article`
+const Article = styled.article`
   padding-top: ${spacingLarge};
   padding-bottom: 12rem;
 `;
+const DEFAULT_TITLE = 'The Store';
+
+function Page({ children, title }) {
+  useEffect(() => {
+    document.title = title ? `${DEFAULT_TITLE} | ${title}` : DEFAULT_TITLE;
+  });
+
+  return <Article>{children}</Article>;
+}
 
 export default Page;
