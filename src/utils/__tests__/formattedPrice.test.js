8b3c3f85933cd237ebb2143c3ef85b1793beeb45
blurbyte <mateusz.lachacz@wipro.com>
2019-04-08 12:14:11 +0200

    Update tests descriptions

--- src/utils/__tests__/formattedPrice.test.js
+++ src/utils/__tests__/formattedPrice.test.js
@@ -5,11 +5,11 @@ describe('FUNC - formattedPrice', () => {
     expect(formattedPrice(1999)).toEqual('$19.99');
   });
 
-  it('formats price and adds currecy sign', () => {
+  it('formats small, cents only price and adds currecy sign', () => {
     expect(formattedPrice(99)).toEqual('$0.99');
   });
 
-  it('formats price and adds currecy sign', () => {
+  it('formats price and adds currecy sign and trailing zeroes', () => {
     expect(formattedPrice(299900)).toEqual('$2999.00');
   });
 });
