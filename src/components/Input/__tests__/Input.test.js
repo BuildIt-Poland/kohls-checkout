94ac984c7c337f00ff23569ce37a5592d3d80b63
Luciano Santos <luciusjss@gmail.com>
2019-04-08 13:47:06 +0200

    FormInput component added

--- src/components/Input/__tests__/Input.test.js
+++ src/components/Input/__tests__/Input.test.js
@@ -4,42 +4,13 @@ import { create } from 'react-test-renderer';
 import Input from '../Input.js';
 
 describe('COMPONENT - Input', () => {
-  describe('Input without error', () => {
-    it('renders correctly Input', () => {
-      const props = {
-        label: 'Dummy Label',
-        placeholder: 'Dummy placeHolder',
-        type: 'text'
-      };
-      const component = create(<Input {...props} />);
+  it('renders correctly Input', () => {
+    const props = {
+      placeholder: 'Dummy placeHolder',
+      type: 'text'
+    };
+    const component = create(<Input {...props} />);
 
-      expect(component.toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Input with error', () => {
-    it('renders correctly Input', () => {
-      const props = {
-        label: 'Dummy Label',
-        placeholder: 'Dummy placeHolder',
-        type: 'text',
-        error: 'dummyError'
-      };
-      const component = create(<Input {...props} />);
-
-      expect(component.toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Input without label', () => {
-    it('renders correctly Input', () => {
-      const props = {
-        placeholder: 'Dummy placeHolder',
-        type: 'text'
-      };
-      const component = create(<Input {...props} />);
-
-      expect(component.toJSON()).toMatchSnapshot();
-    });
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
