315f698f01fe07fffb323db10505b0bb227cb9e1
Luciano Santos <luciusjss@gmail.com>
2019-04-09 10:12:39 +0200

    added unit tests for input with error

--- src/components/Input/__tests__/Input.test.js
+++ src/components/Input/__tests__/Input.test.js
@@ -13,4 +13,15 @@ describe('COMPONENT - Input', () => {
 
     expect(component.toJSON()).toMatchSnapshot();
   });
+
+  it('renders correctly Input with error', () => {
+    const props = {
+      placeholder: 'Dummy placeHolder',
+      type: 'text',
+      error: true
+    };
+    const component = create(<Input {...props} />);
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
