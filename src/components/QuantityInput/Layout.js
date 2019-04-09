8092803377d289d6cb4fd5344e26f449f96dca2b
blurbyte <mateusz.lachacz@wipro.com>
2019-04-09 13:44:10 +0200

    Fix QuantityInput on mobile

--- src/components/QuantityInput/Layout.js
+++ src/components/QuantityInput/Layout.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+import media from '../../styles/media';
+
 const Layout = styled.div`
   display: grid;
   grid-template-columns: 4rem 12rem 4rem;
   height: 4rem;
+
+  ${media.phone`
+    grid-template-columns: 4rem 8rem 4rem;
+  `};
 `;
 
 export default Layout;
