79ca305f75f5cd81bb9c920c4e83f54f14bc0502
blurbyte <mateusz.lachacz@wipro.com>
2019-04-09 11:20:55 +0200

    Remove old Item

--- src/components/Item/Image.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Image = styled.img`
-  width: 100%;
-  grid-column-start: 1;
-  grid-column-end: 2;
-`;
-
-export default Image;
--- src/components/Item/PriceBox.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import { spacingSmall } from '../../styles/designTokens';
-
-const PriceBox = styled.div`
-  margin: ${spacingSmall} 0;
-`;
-
-export default PriceBox;
