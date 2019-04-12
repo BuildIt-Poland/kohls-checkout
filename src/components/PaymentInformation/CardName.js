fd167f948a2b7a58ac6fa711606296a8a47b2c2a
blurbyte <mateusz.lachacz@wipro.com>
2019-04-12 10:41:25 +0200

    Make CardName responsive

--- src/components/PaymentInformation/CardName.js
+++ src/components/PaymentInformation/CardName.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import { CREDIT_CARD_VISA, CREDIT_CARD_MASTER_CARD, CREDIT_CARD_AM_EX } from '../../constants/creditCardTypes';
+import { spacingSmall } from '../../styles/designTokens';
+import media from '../../styles/media';
 import { creditCardType } from '../../utils';
 import SectionHeaderBase from '../SectionHeader';
 
 const SectionHeader = styled(SectionHeaderBase)`
   margin: 0;
+
+  ${media.phone`
+    margin-top: ${spacingSmall};
+  `};
 `;
 
 export function creditCardName(cardNumber) {
