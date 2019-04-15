a6a2a39a164457d167bfd26aa68078ab14574785
blurbyte <mateusz.lachacz@wipro.com>
2019-04-15 12:30:58 +0200

    Update Text color calculations

--- src/components/Text/__tests__/Text.test.js
+++ src/components/Text/__tests__/Text.test.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
 
-import Text from '../Text';
+import { colorBlack, colorDarkGrey, colorTurquoiseDimmed } from '../../../styles/designTokens';
+import Text, { fontColor } from '../Text';
 
 describe('COMPONENT - Text', () => {
   it('renders Text component', () => {
     const component = create(<Text />);
 
     expect(component.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('FUNC - fontColor', () => {
+  it("return proper color if 'highlight' is passed", () => {
+    expect(fontColor({ highlight: true })).toEqual(colorTurquoiseDimmed);
+  });
+
+  it("return proper color if 'dimmed' is passed", () => {
+    expect(fontColor({ dimmed: true })).toEqual(colorDarkGrey);
+  });
+
+  it("return proper color if both 'highlight' and 'dimmed' are passed", () => {
+    expect(fontColor({ highlight: true, dimmed: true })).toEqual(colorTurquoiseDimmed);
+  });
+
+  it("return proper color if 'highlight' is passed", () => {
+    expect(fontColor({ highlight: false, dimmed: false })).toEqual(colorBlack);
   });
 });
