27b06cefc553c683d494897acd7c8eef18c6c0e3
blurbyte <mateusz.lachacz@wipro.com>
2019-04-15 10:03:02 +0200

    Improve NextStep component

--- src/components/NextStep/NextStep.js
+++ src/components/NextStep/NextStep.js
@@ -7,19 +7,26 @@ import Button from './Button';
 import Wrapper from './Wrapper';
 
 function NextStep({ label = 'Next step', to, onMoveToNextStep, disabled }) {
+  const renderButtonOrLink = () => {
+    if (to) {
+      return (
+        <Button as={Link} data-testid="next-step-button" to={to} disabled={disabled}>
+          {label}
+        </Button>
+      );
+    } else if (onMoveToNextStep) {
+      return (
+        <Button data-testid="next-step-button" onClick={onMoveToNextStep} disabled={disabled}>
+          {label}
+        </Button>
+      );
+    } else {
+      throw new Error("You must provided either 'to' or 'onMoveToNextStep' prop.");
+    }
+  };
   return (
     <ErrorBoundary>
-      <Wrapper>
-        {to ? (
-          <Button as={Link} data-testid="next-step-button" to={to} disabled={disabled}>
-            {label}
-          </Button>
-        ) : (
-          <Button data-testid="next-step-button" onClick={onMoveToNextStep} disabled={disabled}>
-            {label}
-          </Button>
-        )}
-      </Wrapper>
+      <Wrapper>{renderButtonOrLink()}</Wrapper>
     </ErrorBoundary>
   );
 }
