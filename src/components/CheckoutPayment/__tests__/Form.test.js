e0ca2234880c5bd50abfe6bead84e1beadbc8acf
Luciano Santos <luciusjss@gmail.com>
2019-04-16 12:55:53 +0200

    Mock optiomns builder in paymentForm

--- src/components/CheckoutPayment/__tests__/Form.test.js
+++ src/components/CheckoutPayment/__tests__/Form.test.js
@@ -7,6 +7,7 @@ jest.mock('../../FormInput', () => 'FormInput');
 jest.mock('../../FormCreditCardInput', () => 'FormCreditCardInput');
 jest.mock('../../FormSelect', () => 'FormSelect');
 jest.mock('../../Text', () => 'Text');
+jest.mock('../optionsBuilder', () => ({ generateMonths: () => [], generateYears: () => [] }));
 
 describe('COMPONENT - CheckoutPayment form', () => {
   it('renders correctly', () => {
