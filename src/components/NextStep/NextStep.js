0a8ad15a849167f8aeaaa00a62510b8112f57be9
blurbyte <mateusz.lachacz@wipro.com>
2019-04-16 13:21:42 +0200

    Make NextStep a bit more compact

--- src/components/NextStep/NextStep.js
+++ src/components/NextStep/NextStep.js
@@ -7,26 +7,19 @@ import Button from './Button';
 import Wrapper from './Wrapper';
 
 function NextStep({ label = 'Next step', to, onMoveToNextStep, disabled }) {
-  const renderButtonOrLink = () => {
-    if (to) {
-      return (
-        <Button as={Link} data-testid="next-step-button" to={to} disabled={disabled}>
-          {label}
-        </Button>
-      );
-    } else if (onMoveToNextStep) {
-      return (
-        <Button data-testid="next-step-button" onClick={onMoveToNextStep} disabled={disabled}>
-          {label}
-        </Button>
-      );
-    } else {
-      throw new Error("You must provided either 'to' or 'onMoveToNextStep' prop.");
-    }
+  // Render link if 'to' prop is passed, button with handler otherwise
+
+  const props = {
+    ...(to ? { as: Link, to } : { onClick: onMoveToNextStep })
   };
+
   return (
     <ErrorBoundary>
-      <Wrapper>{renderButtonOrLink()}</Wrapper>
+      <Wrapper>
+        <Button data-testid="next-step-button" disabled={disabled} {...props}>
+          {label}
+        </Button>
+      </Wrapper>
     </ErrorBoundary>
   );
 }
