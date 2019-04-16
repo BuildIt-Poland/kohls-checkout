c93e5cb3ab23626c9fe99d3a1cb713ab2fc438a5
blurbyte <mateusz.lachacz@wipro.com>
2019-04-16 10:49:25 +0200

    Update CartItemControls props

--- src/components/CartItemControls/__tests__/CartItemControls.test.js
+++ src/components/CartItemControls/__tests__/CartItemControls.test.js
@@ -6,9 +6,11 @@ import CartItemControls from '../CartItemControls';
 describe('COMPONENT - CartItemControls', () => {
   it('renders correctly', () => {
     const props = {
+      id: 'test-id-123',
       quantity: 1,
       increaseQuantity: jest.fn(),
-      decreaseQuantity: jest.fn()
+      decreaseQuantity: jest.fn(),
+      removeItem: jest.fn()
     };
 
     const { getByTestId } = render(<CartItemControls {...props} />);
