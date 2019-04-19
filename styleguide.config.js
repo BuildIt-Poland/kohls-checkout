7fd4407f4d9660d2385a81c85d7295f98896e32f
Marcin Goławski <marcin.golawski@wipro.com>
2019-04-19 13:35:29 +0200

    Adds Error props documentation

--- styleguide.config.js
+++ styleguide.config.js
@@ -78,7 +78,7 @@ module.exports = {
       name: 'Feedback',
       components: [
         'src/components/Text/index.js',
-        'src/components/Error/index.js',
+        'src/components/Error/Error.js',
         'src/components/ModalTrigger/index.js'
       ],
     }
