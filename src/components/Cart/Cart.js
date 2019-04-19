db24e100b82c06ffaea90938583f10e172bc86c0
Marcin Goławski <marcin.golawski@wipro.com>
2019-04-19 09:49:16 +0200

    Eslint fix

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import noop from 'lodash.noop';
 
 import { CHECKOUT_DELIVERY_PATH } from '../../constants/routes';
 import { item } from '../../types';
