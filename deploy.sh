9f6904ff5d246d89809cdb5cd331aae69ec72df8
maciejhein <45594475+maciejhein@users.noreply.github.com>
2019-04-19 10:27:21 +0200

    Fix styleguide deploy (#78)

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,14 @@ npm install
 # --user option tells to install the program to a subdirectory of your user directory to avoid modifying
 # libraries used by your operating system.
 pip install awscli --upgrade --user
-if [ $1 == 'styleguide' ]
-then
-  npx styleguidist build
 
-  # Sync our build folder with our S3 bucket
-  # --acl public-read says deploy the files with public read access
-  # --delete says to delete files in the bucket that aren't present in the build folder
-  aws s3 sync styleguide/ "s3://buildit-store-styleguide" --acl public-read --delete
-else
-  npm run build
+npm run build
 
-  aws s3 sync build/ "s3://buildit-store" --acl public-read --delete
-fi
+# Sync our build folder with our S3 bucket
+# --acl public-read says deploy the files with public read access
+# --delete says to delete files in the bucket that aren't present in the build folder
+aws s3 sync build/ "s3://buildit-store" --acl public-read --delete
+
+npx styleguidist build
+
+aws s3 sync styleguide/ "s3://buildit-store-styleguide" --acl public-read --delete
