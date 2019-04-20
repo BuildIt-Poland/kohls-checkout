047fe6c7e07dc6cca9781609086f43d6400867df
blurbyte <mateusz.lachacz@wipro.com>
2019-04-20 22:42:17 +0200

    Add styleguide related npm scripts

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ npm run build
 # --delete says to delete files in the bucket that aren't present in the build folder
 aws s3 sync build/ "s3://buildit-store" --acl public-read --delete
 
-npx styleguidist build
+npm run styleguide:build
 
 aws s3 sync styleguide/ "s3://buildit-store-styleguide" --acl public-read --delete
